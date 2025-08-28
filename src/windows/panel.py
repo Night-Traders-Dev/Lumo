10db7573a4b028ae2a77f2a0f9d6aa7a78cbbf96
root <root@localhost.localdomain>
2025-08-28 21:35:50 +0000

    panel fixes

--- src/windows/panel.py
+++ src/windows/panel.py
@@ -6,7 +6,6 @@ from shell.utils import get_display_geo, get_active_network, get_battery
 from styles.styles import load_css
 
 
-
 class TopPanel(Gtk.Window):
     def __init__(self, shell):
         super().__init__(title="Panel")
@@ -24,24 +23,34 @@ class TopPanel(Gtk.Window):
         box.set_name("panel")
         self.add(box)
 
-#        self.menu_btn = Gtk.Button.new_with_label("≡")
-#        self.menu_btn.connect("clicked", lambda *_: self.shell.toggle())
-#        box.pack_start(self.menu_btn, False, False, 8)
-
+        # Center clock
         self.clock = Gtk.Label(label="--:--")
         self.clock.get_style_context().add_class("title")
         box.set_center_widget(self.clock)
-        GLib.timeout_add_seconds(1, self._tick)
 
+        # Right info (battery + network)
         right = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=12)
+        self.sysinfo = Gtk.Label(label="🔋 --%   📶 --")
         netType = get_active_network()
         batteryLevel = get_battery()
-        right_lbl = Gtk.Label(label=f"🔋 {batteryLevel}%   📶 {netType}")
-        right.pack_end(right_lbl, False, False, 12)
+        self.sysinfo.set_text(f"🔋 {batteryLevel}%   📶 {netType}")
+        right.pack_end(self.sysinfo, False, False, 12)
         box.pack_end(right, False, False, 8)
+
+        # Start periodic updates
+        GLib.timeout_add_seconds(1, self._tick)
 
         self.show_all()
 
     def _tick(self):
+        # Update clock
         self.clock.set_text(time.strftime("%H:%M"))
+
+        # Update system info (every 10s)
+        now = int(time.time())
+        if now % 60 == 0:
+            netType = get_active_network()
+            batteryLevel = get_battery()
+            self.sysinfo.set_text(f"🔋 {batteryLevel}%   📶 {netType}")
+
         return True
