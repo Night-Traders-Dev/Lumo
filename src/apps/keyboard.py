842a6d9e762efb97f20f400c1012f8dd68131673
root <root@localhost.localdomain>
2025-08-28 17:31:18 +0000

    keyboard fixes

--- src/apps/keyboard.py
+++ src/apps/keyboard.py
@@ -8,8 +8,9 @@ _keyboard_window = None
 
 # Screen geometry and sizing factors
 sw, sh = get_display_geo()
-HEIGHT_MULTIPLIER = 0.35
-WIDTH_MULTIPLIER  = 0.70
+HEIGHT_MULTIPLIER = 0.25
+WIDTH_MULTIPLIER  = 1.0
+
 
 class VirtualKeyboard(Gtk.Window):
     def __init__(self):
@@ -18,7 +19,13 @@ class VirtualKeyboard(Gtk.Window):
         self.set_decorated(False)
         self.set_resizable(False)
         self.set_accept_focus(False)
-        self.set_default_size(int(sw * WIDTH_MULTIPLIER), int(sh * HEIGHT_MULTIPLIER))
+        self.set_border_width(10)
+
+
+        # Fixed keyboard size
+        self.kb_width  = int(sw * WIDTH_MULTIPLIER)
+        self.kb_height = int(sh * HEIGHT_MULTIPLIER)
+        self.set_default_size(self.kb_width, self.kb_height)
 
         self.shift = False
         self.ctrl = False
@@ -30,12 +37,8 @@ class VirtualKeyboard(Gtk.Window):
 
         # Grid container for keys
         self.grid = Gtk.Grid()
-        self.grid.set_column_homogeneous(True)
-        self.grid.set_row_homogeneous(True)
         self.grid.set_column_spacing(0)
         self.grid.set_row_spacing(0)
-        self.grid.set_hexpand(False)
-        self.grid.set_vexpand(True)
         self.vbox.pack_start(self.grid, True, True, 0)
 
         # Full QWERTY layout
@@ -46,16 +49,15 @@ class VirtualKeyboard(Gtk.Window):
             ["Ctrl","Space","Enter","Backspace"]
         ]
 
-        screen = Gdk.Screen.get_default()
-        if screen:
-            self.create_keys()
-            self.connect("size-allocate", self.on_size_allocate)
+        self.create_keys()
+        self.connect("size-allocate", self.on_size_allocate)
+
 
     def create_keys(self):
         """Create all key buttons dynamically with proportional sizing"""
         self.grid.foreach(lambda w: self.grid.remove(w))
-        kb_height = int(sh * HEIGHT_MULTIPLIER)
-        row_height = int(kb_height / len(self.keys_layout))
+
+        row_height = int(self.kb_height / len(self.keys_layout))
 
         for r, key_row in enumerate(self.keys_layout):
             col = 0
@@ -65,23 +67,27 @@ class VirtualKeyboard(Gtk.Window):
                 btn.connect("pressed", self.on_key_pressed, key)
                 btn.connect("released", self.on_key_released)
 
+                # Proportional spans
                 if key == "Space":
-                    self.grid.attach(btn, col, r, 5, 1)   # wide space bar
-                    col += 5
+                    span = 5
                 elif key in ["Ctrl", "Enter", "Backspace", "Shift"]:
-                    self.grid.attach(btn, col, r, 2, 1)   # wider specials
-                    col += 2
+                    span = 2
                 else:
-                    self.grid.attach(btn, col, r, 1, 1)   # normal keys
-                    col += 1
+                    span = 1
+
+                self.grid.attach(btn, col, r, span, 1)
+                col += span
+
 
     def on_size_allocate(self, widget, allocation):
-        """Dock keyboard at bottom-left, shrink from right."""
-        kb_height = int(sh * HEIGHT_MULTIPLIER)
-        kb_width = int(sw * WIDTH_MULTIPLIER)
-        widget.resize(kb_width, kb_height)
-        widget.move(0, sh - kb_height)
-        self.grid.set_size_request(kb_width, -1)
+        """Dock keyboard at bottom-center with fixed size"""
+        self.resize(self.kb_width, self.kb_height)
+        x_pos = int((sw - self.kb_width) / 2)
+        y_pos = sh - self.kb_height
+        self.move(x_pos, y_pos)
+        self.set_size_request(self.kb_width, self.kb_height)
+        self.grid.set_size_request(self.kb_width, self.kb_height)
+
 
     def send_key(self, key):
         args = ["xdotool"]
@@ -106,6 +112,7 @@ class VirtualKeyboard(Gtk.Window):
                 args += ["key", key.lower()]
         subprocess.run(args)
 
+
     def update_shift_appearance(self):
         # Optional: visually toggle Shift state
         pass
@@ -114,9 +121,11 @@ class VirtualKeyboard(Gtk.Window):
         # Optional: visually toggle Ctrl state
         pass
 
+
     def repeat_key(self, key):
         self.send_key(key)
         self.repeat_id = GLib.timeout_add(100, self.repeat_key, key)
+
 
     def on_key_pressed(self, widget, key):
         if key == "Shift":
@@ -130,15 +139,19 @@ class VirtualKeyboard(Gtk.Window):
             if key not in ["Shift", "Ctrl"]:
                 self.repeat_id = GLib.timeout_add(400, self.repeat_key, key)
 
+
     def on_key_released(self, widget):
         if self.repeat_id:
             GLib.source_remove(self.repeat_id)
             self.repeat_id = None
 
+
     def on_close(self, widget=None):
         global _keyboard_window
         self.destroy()
         _keyboard_window = None
+
+
 
 def launch():
     """Toggle keyboard visibility"""
